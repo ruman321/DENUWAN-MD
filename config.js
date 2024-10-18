61b1cdb597178a967e63fe0b5e66cd9201d3fad5
ruman321 <ruman.mail421@gmail.com>
2024-10-18 17:53:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "dmEHmb5D#vxDN8A4rMCvrtfUUJyQMeZUA7MZyOsNkDkvMZlkRGWU",
+SESSION_ID: process.env.SESSION_ID || "V3sjwJDb#KNwxMhCYK_C1E1PN3laKhqKo-FTqApNpywgjNA1bSIY",
 MONGODB: process.env.MONGODB || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://unitedcamps.in/Images/file_4265.jpg",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
